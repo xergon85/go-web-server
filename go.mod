1e81e1f7cf3faf7043b1887b264be116b55158c6
xergon85 <christoffer.isenberg@gmail.com>
2024-06-27 13:27:02 +0200

    Added gorilla for routing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xergon85/go-web-server
 
 go 1.22.3
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,17 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Helllo, you've requested: %s\n", r.URL.Path)
-	})
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	router, err := createRouter()
+	if err != nil {
+		panic(err)
+	}
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", router)
+}
+
+func createRouter() (*mux.Router, error) {
+	r := mux.NewRouter()
+
+	handleStatic(r)
+	r.HandleFunc("/", indexGet)
+	r.HandleFunc("/books/{title}/page/{page}", bookTitlePage)
+
+	return r, nil
+}
+
+func indexGet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Helllo, you've requested: %s\n", r.URL.Path)
+}
+
+func handleStatic(r *mux.Router) error {
+	var dir string
+
+	flag.StringVar(&dir, dir, "static", "the directory to serve files from. Defaults to the /static")
+	flag.Parse()
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir(dir))))
+
+	return nil
+}
+
+func bookTitlePage(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	page := vars["page"]
+
+	fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
 }
